45134c16d30b50709ec03db5b54620ab909994cb
kimtree98 <65067460+kimtree98@users.noreply.github.com>
2020-11-30 20:11:21 +0900

    알람 코드 수정
    수정내용: 1.정상 종료 코드구현, 2. 문장으로 자연스럽게 시간처리

--- Alarm.cpp
+++ Alarm.cpp
@@ -1,7 +1,7 @@
 #include "Alarm.h"
 #include "DirectoryManager.h"
 
-
+using namespace std;
 
 // 현재시각
 void Alarm::Nowtime()
@@ -32,27 +32,78 @@ int Alarm::Time_cal(){
 
 }
 
+int Alarm::Time_inputstring(){
 
 
-void Alarm::Alarm_function()
+    strcpy(str_buff, insert.c_str());
+
+    char *tok = strtok(str_buff, " ");
+    while (tok != nullptr) {
+        str_arr[str_cnt++] = string(tok);
+        tok = strtok(nullptr, " ");
+    }
+
+
+    string strhr=str_arr[0];
+    string strmin=str_arr[1];
+    string strsec=str_arr[2];
+
+    hr = stoi(strhr);
+    min = stoi(strmin);
+    sec = stoi(strsec);
+
+    return hr, min, sec;
+
+
+}
+
+
+
+
+int Alarm::Alarm_function()
 {
-    Nowtime();
-    std::cout<<"설정 시간을 입력해주세요 (시 분 초 모두 입력): ";
-    std::cout<<">>";
-    std::cin>> hr >> min >>sec;
-    std::cout<<"설정 시간을 저장할까요?";
+    
+    
+    while (1)
+    {
+        Nowtime();
+       
+        std::cout<<"설정 시간을 입력해주세요 [시 분 초 모두 입력해주세요. (ex.4초 = 0시간 0분 4초 타이머 설정해줘)] ";
+        std::cout<<">>";
+        getline(cin,insert);
 
-    std::stringstream ss;
-    ss.str(insert);
-    ss>>hr;
-    ss>>min;
-    ss>>sec;
-    Time_cal();
+        Time_inputstring();
 
-    std::cout << hr <<"시간"<< min<<"분"<<sec<< "초 타이머를 설정했어요!"<<'\n';
-    Nowtime();
-    std::this_thread::sleep_for (std::chrono::seconds(total));// 스레드 활용
-    std::cout << hr<<"시간"<< min<<"분"<<sec<< "초 타이머가 종료되었습니다."<<'\n';
-    Nowtime();
+        Time_cal();
+
+        std::cout << hr <<"시간"<< min<<"분"<<sec<< "초 타이머를 설정했어요!"<<'\n';
+        Nowtime();
+        std::this_thread::sleep_for (std::chrono::seconds(total));// 스레드 활용
+        std::cout << hr<<"시간"<< min<<"분"<<sec<< "초 타이머가 종료되었습니다."<<'\n';
+        Nowtime();
+        char input;
+
+        cout << "알람 기능을 계속 수행할까요? (y: 계속 | n: 종료) " << endl;
+        cin >> input;
+        if (input=='y' || input=='Y'){
+            continue;       
+        }
+        else if (input=='n' || input=='N' )
+        {
+            cout << "알람 도움이 필요하면 언제든지 불러주세요.";
+            cout << endl;
+            break;
+            
+        }
+        else{
+            cout << "다시 입력해주세요!";
+        }
+        
+
+    }
+    return 0;
+    
+    
+
 
 }
--- Alarm.h
+++ Alarm.h
@@ -1,3 +1,6 @@
+#ifndef __ALARM_H__
+#define __ALARM_H__
+
 #include <fstream>
 #include <iostream>
 #include <chrono>
@@ -6,6 +9,10 @@
 #include <sstream>
 #include <ctime>
 #include <cstring>
+#include <stdlib.h>
+#include <cstdlib>
+
+using namespace std;
 
 class Alarm
 {
@@ -15,14 +22,20 @@ private:
     int hr2;
     int min2;
     int sec;
-    char insert[100]; 
+    string insert;
     int total;
     time_t now_time; 
     struct tm *now_date; 
     char buf[100];
+    int str_cnt;
+    string str_arr[1000];
+    char *str_buff = new char[1000];
 public:
     char *Alarm_path;
     void Nowtime();// 현재시간 구하는 함수
-    void Alarm_function();// 알람시계 메인 구동함수
+    int Alarm_function();// 알람시계 메인 구동함수
     int Time_cal();// 입럭 값 Sleep()으로 보내기 위한 시간 변환 함수
+    int Time_inputstring();// 사용자 원하는 시간 입력값 String Parsing 처리 함수
 };
+
+#endif
